0fe30ab5b4b00cd6c36750e4f47944468d651a5f
696969 <tinkertechinc@gmail.com>
2018-04-11 14:32:02 +0800

    best seller slide

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { ProductService } from '../../providers/product';
 
 @Component({
   selector: 'page-about',
   templateUrl: 'about.html'
 })
 export class AboutPage {
+  public bestSellerProducts = [];
 
-  constructor(public navCtrl: NavController) {
+  constructor(public navCtrl: NavController, private productService: ProductService) {
 
   }
+
+  ionViewDidLoad(){
+   this.productService.getProducts()
+   .subscribe((allProducts) => {
+      this.bestSellerProducts = allProducts.filter(
+        product => product.bestSeller == true);
+        console.log(this.bestSellerProducts);
+   });
+  }
+  
 
 }
